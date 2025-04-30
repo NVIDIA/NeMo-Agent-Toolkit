097dd755cebe0340cde790ad992c43d8c080bdec
David Gardner <dagardner@nvidia.com>
2025-04-30 14:18:59 -0700

    Remove temporary hard-coded variable
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- ci/scripts/github/build_wheel.sh
+++ ci/scripts/github/build_wheel.sh
@@ -52,7 +52,6 @@ for AIQ_PACKAGE in "${AIQ_PACKAGES[@]}"; do
     build_package_wheel ${AIQ_PACKAGE}
 done
 
-BUILD_AIQ_COMPAT="true"
 if [[ "${BUILD_AIQ_COMPAT}" == "true" ]]; then
     for AIQ_COMPAT_PACKAGE in "${AIQ_COMPAT_PACKAGES[@]}"; do
         build_package_wheel ${AIQ_COMPAT_PACKAGE}
