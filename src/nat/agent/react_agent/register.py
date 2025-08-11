d531992793a4ef5b3a3e468759ba93933d97c859
Yuchen Zhang <yuchenz@nvidia.com>
2025-08-11 16:46:29 -0700

    remove use_openai_api
    Signed-off-by: Yuchen Zhang <yuchenz@nvidia.com>

--- src/nat/agent/react_agent/register.py
+++ src/nat/agent/react_agent/register.py
@@ -65,9 +65,9 @@ class ReActAgentWorkflowConfig(FunctionBaseConfig, name="react_agent"):
         default=None,
         description="Provides the SYSTEM_PROMPT to use with the agent")  # defaults to SYSTEM_PROMPT in prompt.py
     max_history: int = Field(default=15, description="Maximum number of messages to keep in the conversation history.")
-    use_openai_api: bool = Field(default=False,
-                                 description=("Use OpenAI API for the input/output types to the function. "
-                                              "If False, strings will be used."))
+    use_openai_api: bool = Field(default=True,
+                                 description=("This option is deprecated and will be removed in a future release. "
+                                              "This option will NOT take any effect."))
     additional_instructions: str | None = Field(
         default=None, description="Additional instructions to provide to the agent in addition to the base prompt.")
 
@@ -135,15 +135,4 @@ async def react_agent_workflow(config: ReActAgentWorkflowConfig, builder: Builde
                 return ChatResponse.from_string(str(ex))
             return ChatResponse.from_string("I seem to be having a problem.")
 
-    if (config.use_openai_api):
-        yield FunctionInfo.from_fn(_response_fn, description=config.description)
-    else:
-
-        async def _str_api_fn(input_message: str) -> str:
-            oai_input = GlobalTypeConverter.get().try_convert(input_message, to_type=ChatRequest)
-
-            oai_output = await _response_fn(oai_input)
-
-            return GlobalTypeConverter.get().try_convert(oai_output, to_type=str)
-
-        yield FunctionInfo.from_fn(_str_api_fn, description=config.description)
+    yield FunctionInfo.from_fn(_response_fn, description=config.description)
