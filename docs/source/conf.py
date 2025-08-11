446aab0d7c29d6a315c5d078ff66134f1980f5c9
David Gardner <dagardner@nvidia.com>
2025-08-11 09:33:59 -0700

    Rename in docs source
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ with open(os.path.join(API_TREE, "aiq", "__init__.py"), "w") as f:
 
 # -- Project information -----------------------------------------------------
 
-project = 'NVIDIA Agent Intelligence Toolkit'
+project = 'NVIDIA NeMo Agent Toolkit'
 copyright = '2025, NVIDIA'
 author = 'NVIDIA Corporation'
 
@@ -238,14 +238,14 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (root_doc, 'aiq.tex', 'Agent Intelligence Toolkit Documentation', 'NVIDIA', 'manual'),
+    (root_doc, 'nat.tex', 'NeMo Agent Toolkit Documentation', 'NVIDIA', 'manual'),
 ]
 
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(root_doc, 'aiq', 'Agent Intelligence Toolkit Documentation', [author], 1)]
+man_pages = [(root_doc, 'nat', 'NeMo Agent Toolkit Documentation', [author], 1)]
 
 # -- Options for Texinfo output ----------------------------------------------
 
@@ -254,10 +254,10 @@ man_pages = [(root_doc, 'aiq', 'Agent Intelligence Toolkit Documentation', [auth
 #  dir menu entry, description, category)
 texinfo_documents = [
     (root_doc,
-     'aiq',
-     'Agent Intelligence Toolkit Documentation',
+     'nat',
+     'NeMo Agent Toolkit Documentation',
      author,
-     'aiq',
+     'nat',
      'One line description of project.',
      'Miscellaneous'),
 ]
