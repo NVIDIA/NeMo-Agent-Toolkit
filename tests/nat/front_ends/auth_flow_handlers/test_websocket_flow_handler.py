b5610b7688dede00d977fb0d1e359de62029b334
David Gardner <dagardner@nvidia.com>
2025-09-08 12:10:05 -0700

    Remove unused import
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- tests/nat/front_ends/auth_flow_handlers/test_websocket_flow_handler.py
+++ tests/nat/front_ends/auth_flow_handlers/test_websocket_flow_handler.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import socket
 from urllib.parse import parse_qs
 from urllib.parse import urlparse
