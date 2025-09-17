9ba07e1bd280969dda9561f58fb91c02ca1ffba2
Will Killian <wkillian@nvidia.com>
2025-09-17 11:55:52 -0400

    chore: add copyright
    Signed-off-by: Will Killian <wkillian@nvidia.com>

--- tests/nat/builder/test_function_group.py
+++ tests/nat/builder/test_function_group.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from collections.abc import Sequence
 
 import pytest
@@ -7,8 +22,6 @@ from nat.builder.function import FunctionGroup
 from nat.builder.function import LambdaFunction
 from nat.data_models.function import EmptyFunctionConfig
 from nat.data_models.function import FunctionGroupBaseConfig
-
-# ========== FunctionGroup Filter Tests ==========
 
 
 class FunctionGroupTestConfig(FunctionGroupBaseConfig, name="test_function_group"):
