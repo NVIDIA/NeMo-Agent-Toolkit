ddc6b5391a9e466026c471b674e7385283264272
David Gardner <dagardner@nvidia.com>
2025-09-24 09:41:29 -0700

    Avoid calling isinstance when the type is typing.Annotated
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- src/nat/builder/component_utils.py
+++ src/nat/builder/component_utils.py
@@ -158,7 +158,9 @@ def recursive_componentref_discovery(cls: TypedBaseModel, value: typing.Any,
             yield from recursive_componentref_discovery(cls, field_data, field_info.annotation)
     if (decomposed_type.is_union):
         for arg in decomposed_type.args:
-            if arg is typing.Any or (isinstance(value, DecomposedType(arg).root)):
+            decomposed_root = DecomposedType(arg).root
+            if decomposed_root is not typing.Annotated and (arg is typing.Any or arg is typing.Annotated or
+                                                            (isinstance(value, decomposed_root))):
                 yield from recursive_componentref_discovery(cls, value, arg)
     else:
         for arg in decomposed_type.args:
