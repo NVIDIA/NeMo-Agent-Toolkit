8e96bd151ba2faa9b9f198e9aa51c211855fb686
David Gardner <dagardner@nvidia.com>
2025-10-14 09:31:39 -0700

    First pass at memory example test
    Signed-off-by: David Gardner <dagardner@nvidia.com>

---
+++ examples/memory/redis/tests/test_memory_redis.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+
+import pytest
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures("nvidia_api_key")
+async def test_full_workflow(phoenix_trace_url: str, examples_dir: Path):
+    from nat.runtime.loader import load_config
+    from nat.test.utils import locate_example_config
+    from nat.test.utils import run_workflow
+
+    config_file = (examples_dir / "memory/redis/configs/config.yml")
+
+    config = load_config(config_file)
+    config.general.telemetry.tracing["phoenix"].endpoint = phoenix_trace_url
+
+    await run_workflow(config=config, question="my favorite flavor is strawberry", expected_answer="strawberry")
+    await run_workflow(config=config, question="what flavor of ice-cream should I get?", expected_answer="strawberry")
