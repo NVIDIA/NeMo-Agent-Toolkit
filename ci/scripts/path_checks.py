dc4865703ed846348faa484b2966ed344bb98a67
David Gardner <dagardner@nvidia.com>
2025-11-07 11:11:14 -0800

    Revert "Add SECURITY.md document (#1164)"
    This reverts commit 7d947555575c088c31d5e177965e0f8ce7b6a490.
    
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- ci/scripts/path_checks.py
+++ ci/scripts/path_checks.py
@@ -114,7 +114,6 @@ ALLOWLISTED_WORDS: set[str] = {
     "Input/Observation",
     "input/output",
     "inputs/outputs",
-    "Input/output",
     "JavaScript/TypeScript",
     "JSON/YAML",
     "LangChain/LangGraph",
