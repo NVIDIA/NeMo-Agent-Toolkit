b83c732d45810c00112f102e55ada0bf60d43a5f
Abdullahi Olaoye <aolaoye@nvidia.com>
2025-11-12 12:48:10 -0600

    Add Bedrock Agentcore folder
    Signed-off-by: Abdullahi Olaoye <aolaoye@nvidia.com>

---
+++ examples/frameworks/strands_demo/bedrock_agentcore/scripts/test_nat.py
@@ -0,0 +1,17 @@
+import json
+
+import boto3
+
+client = boto3.client('bedrock-agentcore', region_name='<AWS_REGION>')
+payload = json.dumps({"inputs" : "How do I use the Strands Agents API?"})
+
+response = client.invoke_agent_runtime(
+    agentRuntimeArn='arn:aws:bedrock-agentcore:<AWS_REGION>:<AWS_ACCOUNT_ID>:runtime/<AGENT_RUNTIME_ID>',
+    #    runtimeSessionId='<RUNTIME_SESSION_ID>',  # Must be 33+ chars
+    payload=payload,
+    qualifier="DEFAULT"  # Optional
+)
+response_body = response['response'].read()
+response_data = json.loads(response_body)
+print("Agent Response:", response_data)
+print("Agent Response:", response_data)
---
+++ examples/frameworks/strands_demo/bedrock_agentcore/scripts/update_nat.py
@@ -0,0 +1,17 @@
+import boto3
+
+client = boto3.client('bedrock-agentcore-control', region_name='<AWS_REGION>')
+
+response = client.update_agent_runtime(agentRuntimeId='<AGENT_RUNTIME_ID>',
+                                       agentRuntimeArtifact={
+                                           'containerConfiguration': {
+                                               'containerUri':
+                                                   '<AWS_ACCOUNT_ID>.dkr.ecr.<AWS_REGION>.amazonaws.com/strands-demo:latest'
+                                           }
+                                       },
+                                       networkConfiguration={"networkMode": "PUBLIC"},
+                                       roleArn='<IAM_AGENTCORE_ROLE>')
+
+print(f"Agent Runtime created successfully!")
+print(f"Agent Runtime ARN: {response['agentRuntimeArn']}")
+print(f"Status: {response['status']}")
