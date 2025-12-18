4cb53df020354f65a9c6ed2640703fa9815af004
Eric Evans <194135482+ericevans-nv@users.noreply.github.com>
2025-12-18 12:28:30 -0500

    Updating plot check ruff formatting.
    Signed-off-by: Eric Evans <194135482+ericevans-nv@users.noreply.github.com>

--- src/nat/profiler/calc/plot.py
+++ src/nat/profiler/calc/plot.py
@@ -20,7 +20,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from nat.profiler.calc.data_models import LinearFitResult, SizingMetrics
+from nat.profiler.calc.data_models import LinearFitResult
+from nat.profiler.calc.data_models import SizingMetrics
 
 logger = logging.getLogger(__name__)
 
