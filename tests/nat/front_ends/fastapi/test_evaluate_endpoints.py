a925c5a11b31bac93088fe952eedc78d03c6a5aa
David Gardner <dagardner@nvidia.com>
2026-01-27 11:45:34 -0800

    Remove unused import
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- tests/nat/front_ends/fastapi/test_evaluate_endpoints.py
+++ tests/nat/front_ends/fastapi/test_evaluate_endpoints.py
@@ -17,7 +17,6 @@ import shutil
 import typing
 from pathlib import Path
 from unittest.mock import MagicMock
-from unittest.mock import PropertyMock
 from unittest.mock import patch
 
 import pytest
