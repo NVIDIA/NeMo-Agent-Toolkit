fb8343fa0bb6fc2e1a0b7a0b42072cf29cc187cf
Anuradha Karuppiah <anuradhak@nvidia.com>
2026-02-16 16:27:26 -0800

    Update compat tests
    Signed-off-by: Anuradha Karuppiah <anuradhak@nvidia.com>

--- packages/nvidia_nat_core/tests/nat/test_eval_deprecation_shim.py
+++ packages/nvidia_nat_core/tests/nat/test_eval_deprecation_shim.py
@@ -24,7 +24,7 @@ def test_nat_eval_shim_warns_and_reexports_models():
     with pytest.warns(UserWarning, match="deprecated"):
         importlib.import_module("nat.eval")
 
-    from nat.data_models.evaluate_runtime import EvaluationRunConfig as CoreEvaluationRunConfig
-    from nat.eval.config import EvaluationRunConfig as ShimEvaluationRunConfig
+    from nat.data_models.evaluator import EvalInputItem as CoreEvalInputItem
+    from nat.eval.evaluator.evaluator_model import EvalInputItem as ShimEvalInputItem
 
-    assert ShimEvaluationRunConfig is CoreEvaluationRunConfig
+    assert ShimEvalInputItem is CoreEvalInputItem
